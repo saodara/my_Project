e110edf3a39a224fd083393b9544264c9f834d1b
Daradigi <saodara09dr@gmail.com>
2026-01-07 12:52:00 +0700

    Updates

--- assets/PageNotFound-CK5wgsbo.js
+++ assets/PageNotFound-CK5wgsbo.js
@@ -1,4 +1,4 @@
-import{h as s,j as e,a1 as t,k as a}from"./index-Qp3oFRE6.js";const n=s.div`
+import{h as s,j as e,a1 as t,k as a}from"./index-Cw7gRJ0b.js";const n=s.div`
   min-height: 100vh;
 
   .page-not-found-img {
